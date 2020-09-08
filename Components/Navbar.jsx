f2f9347b0a2dc52c3c42cb933a8d82c19d82eece
MPiotrowska <monikapiotrowska27@gmail.com>
2020-09-08 20:21:57 +0200

    Navbar style

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,29 +1,56 @@
 import React from "react";
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css";
 
 function Navbar() {
-  
-    return (
-  <nav className ={styles.column}>
-    <ul className={styles.navList}>
-    <li><a className={styles.navItem}href="#home">Home</a>
-    <ul className={styles.navItemSub} >
-    <li><a className={styles.navItem} href="#news">Our Menu</a></li>
-        <li><a className={styles.navItem} href="#news">Shop</a></li>
-        <li><a className={styles.navItem} href="#contact">Contact</a></li>
-        <li><a className={styles.navItem} href="#about">About</a></li>
-    </ul>
+  return (
+    <nav className={styles.column}>
+      <ul className={styles.navList}>
+        <li className={`${styles.listItem} ${styles.hasChildren}`}>
+          <a className={styles.listItemLink} href="#home">
+            Home
+          </a>
+          <ul className={styles.listItemLinkSub}>
+            <li className={styles.listItemChild}>
+              <a className={styles.listItemChildLink} href="#news">
+                Meet Our Team
+              </a>
+            </li>
+            <li className={styles.listItemChild}>
+              <a className={styles.listItemChildLink} href="#news">
+                What We Offer
+              </a>
+            </li>
+            <li className={styles.listItemChild}>
+              <a className={styles.listItemChildLink} href="#news">
+               Our Process
+              </a>
+            </li>
+            <li className={styles.listItemChild}>
+              <a className={styles.listItemChildLink} href="#about">
+                About
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className={styles.listItem}>
+          <a className={styles.listItemLink} href="#news">
+            Our Menu
+          </a>
+        </li>
+        <li className={styles.listItem}>
+          <a className={styles.listItemLink} href="#news">
+            Shop
+          </a>
+        </li>
+        <li className={styles.listItem}>
+          <a className={styles.listItemLink} href="#contact">
+            Contact
+          </a>
+        </li>
+     
+      </ul>
+    </nav>
+  );
+}
 
-    </li>
-    <li><a className={styles.navItem} href="#news">Our Menu</a></li>
-    <li><a className={styles.navItem} href="#news">Shop</a></li>
-    <li><a className={styles.navItem} href="#contact">Contact</a></li>
-    <li><a className={styles.navItem} href="#about">About</a></li>
-    </ul>
-</nav> 
-
- 
-    );
-  }
-  
-  export default Navbar;
+export default Navbar;
